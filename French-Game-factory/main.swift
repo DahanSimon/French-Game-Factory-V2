38ca95cfbdfb37a1ff0d01126b859f75fe9dcf11
DahanSimon <dahan.simon@hotmail.fr>
2019-09-17 13:17:35 +0200

    Initial Commit

---
+++ French-Game-factory/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  French-Game-factory
+//
+//  Created by Simon Dahan on 17/09/2019.
+//  Copyright © 2019 Simon Dahan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
